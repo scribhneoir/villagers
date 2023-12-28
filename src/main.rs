bbc720fa6a905074fc17c4f09e5939c37365ef9c
Josiah McCracken <scribhneoir21@gmail.com>
2023-12-28 14:37:39 -0500

    feat: update block pallet

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,25 @@ use bevy::prelude::*;
 
 use noise::{NoiseFn, Perlin};
 
-const WORLD_SEED: u32 = 3;
+const WORLD_SEED: u32 = 187756;
 
 const RENDER_SCALE: f32 = 2.0;
-const BLOCK_TEXTURE_SIZE: f32 = 16.0;
+const BLOCK_TEXTURE_SIZE: f32 = 24.0;
 const CHUNK_SIZE: usize = 16; //blocks^3
+const SAMPLE_SCALE: f64 = 0.007;
+const WATER_LEVEL: usize = 4;
 
 // const WORLD_SIZE: usize = 9; //chunks^2
 
-// const AIR = 0;
-// const DIRT = 1;
-// const GRASS = 2;
-// const STONE = 3;
+const DIRT: usize = 0;
+const GRASS: usize = 1;
+const STONE: usize = 2;
+const SAND: usize = 3;
+const LOG: usize = 4;
+const LEAF: usize = 5;
+
+const WATER: usize = 24;
+const WATER_TOP: usize = 25;
 
 fn main() {
     App::new()
@@ -31,6 +38,33 @@ struct Block;
 #[derive(Component)]
 struct Chunk;
 
+fn spawn_block(
+    commands: &mut Commands,
+    texture_atlas_handle: Handle<TextureAtlas>,
+    sprite_index: usize,
+    x: usize,
+    y: usize,
+    z: usize,
+) {
+    commands.spawn((
+        Block,
+        SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            sprite: TextureAtlasSprite::new(sprite_index),
+            transform: Transform::from_translation(Vec3::new(
+                (x as f32 * BLOCK_TEXTURE_SIZE * RENDER_SCALE / 2.0)
+                    - (y as f32 * BLOCK_TEXTURE_SIZE * RENDER_SCALE / 2.0) as f32,
+                -(y as f32 * BLOCK_TEXTURE_SIZE * RENDER_SCALE / 3.0)
+                    - (x as f32 * BLOCK_TEXTURE_SIZE * RENDER_SCALE / 3.0)
+                    + (z as f32 * BLOCK_TEXTURE_SIZE * RENDER_SCALE / (7.0 / 3.0)) as f32,
+                1.0,
+            ))
+            .with_scale(Vec3::splat(RENDER_SCALE)),
+            ..Default::default()
+        },
+    ));
+}
+
 fn startup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
@@ -47,8 +81,8 @@ fn startup(
     let texture_atlas = TextureAtlas::from_grid(
         texture_handle,
         Vec2::splat(BLOCK_TEXTURE_SIZE),
-        2,
-        2,
+        24,
+        24,
         None,
         None,
     );
@@ -60,54 +94,47 @@ fn startup(
     //spawn blocks for chunk
     for x in 0..CHUNK_SIZE {
         for y in 0..CHUNK_SIZE {
-            const SAMPLE_SCALE: f64 = 0.01;
-            let height_noise = perlin
-                .get([x as f64 * SAMPLE_SCALE, y as f64 * SAMPLE_SCALE])
-                .abs();
-
-            let stone_h = (height_noise * CHUNK_SIZE as f64) as usize;
-            let dirt_h = CHUNK_SIZE - stone_h - 1;
+            let height_noise = perlin.get([x as f64 * SAMPLE_SCALE, y as f64 * SAMPLE_SCALE]);
+            let stone_h = ((height_noise * (CHUNK_SIZE as f64)) * 1.2) as usize;
+            let dirt_h = ((stone_h as f64 + (height_noise * 5.0)) * 1.8) as usize;
 
             for z in 0..stone_h {
-                commands.spawn((
-                    Block,
-                    SpriteSheetBundle {
-                        texture_atlas: texture_atlas_handle.clone(),
-                        sprite: TextureAtlasSprite::new(3),
-                        transform: Transform::from_translation(Vec3::new(
-                            (x as f32 * BLOCK_TEXTURE_SIZE * RENDER_SCALE / 2.0)
-                                - (y as f32 * BLOCK_TEXTURE_SIZE * RENDER_SCALE / 2.0) as f32,
-                            -(y as f32 * BLOCK_TEXTURE_SIZE * RENDER_SCALE / 3.0)
-                                - (x as f32 * BLOCK_TEXTURE_SIZE * RENDER_SCALE / 3.0)
-                                + (z as f32 * BLOCK_TEXTURE_SIZE * RENDER_SCALE / (7.0 / 3.0))
-                                    as f32,
-                            1.0,
-                        ))
-                        .with_scale(Vec3::splat(RENDER_SCALE)),
-                        ..Default::default()
-                    },
-                ));
+                spawn_block(&mut commands, texture_atlas_handle.clone(), STONE, x, y, z);
             }
 
             for z in stone_h..dirt_h {
-                commands.spawn((
-                    Block,
-                    SpriteSheetBundle {
-                        texture_atlas: texture_atlas_handle.clone(),
-                        sprite: TextureAtlasSprite::new(2),
-                        transform: Transform::from_translation(Vec3::new(
-                            (x as f32 * BLOCK_TEXTURE_SIZE * RENDER_SCALE / 2.0)
-                                - (y as f32 * BLOCK_TEXTURE_SIZE * RENDER_SCALE / 2.0) as f32,
-                            -(y as f32 * BLOCK_TEXTURE_SIZE * RENDER_SCALE / 3.0)
-                                - (x as f32 * BLOCK_TEXTURE_SIZE * RENDER_SCALE / 3.0)
-                                + (z as f32 * BLOCK_TEXTURE_SIZE * RENDER_SCALE / (7.0 / 3.0))
-                                    as f32,
-                            1.0,
-                        ))
-                        .with_scale(Vec3::splat(RENDER_SCALE)),
-                        ..Default::default()
-                    },
-                ));
+                spawn_block(&mut commands, texture_atlas_handle.clone(), DIRT, x, y, z);
+            }
+            if dirt_h > WATER_LEVEL {
+                spawn_block(
+                    &mut commands,
+                    texture_atlas_handle.clone(),
+                    GRASS,
+                    x,
+                    y,
+                    dirt_h,
+                );
+            } else if dirt_h == WATER_LEVEL {
+                spawn_block(
+                    &mut commands,
+                    texture_atlas_handle.clone(),
+                    SAND,
+                    x,
+                    y,
+                    dirt_h,
+                );
+            } else {
+                for z in dirt_h..WATER_LEVEL {
+                    spawn_block(&mut commands, texture_atlas_handle.clone(), WATER, x, y, z);
+                }
+                spawn_block(
+                    &mut commands,
+                    texture_atlas_handle.clone(),
+                    WATER_TOP,
+                    x,
+                    y,
+                    WATER_LEVEL,
+                );
             }
         }
     }
